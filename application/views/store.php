442bc701c0587a946590809b80a6a583cc1b37b0
K00222558 <43409483+K00222558@users.noreply.github.com>
2018-12-13 12:55:04 +0000

    update

--- application/views/store.php
+++ application/views/store.php
@@ -4,34 +4,10 @@
 <title>W3.CSS Template</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="w3.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>/assests/CSS/w3.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>/assests/CSS/style.css">
+<script rel="text/javascript" src="<?php echo base_url(); ?>/assests/script/navs.js"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-    .sidenav{
-        display: none;
-    }
-  
-    .dropdown-btn {
-    
-    display: block;
-    border: none;
-}
-.dropdown-container {
-    display: none;
-    background-color: #262626;
-    padding-left: 8px;
-}
-body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
-.w3-row-padding img {margin-bottom: 12px}
-/* Set the width of the sidebar to 120px */
-.w3-sidebar {width: 120px;background: #222}
-/* Add a left margin to the "page content" that matches the width of the sidebar (120px) */
-#main {margin-left: 120px}
-/* Remove margins from "page content" on small screens */
-@media only screen and (max-width: 600px) {#main {margin-left: 0}}
-</style>
 <body class="w3-black">
 
 <!-- Icon Bar (Sidebar - hidden on small screens) -->
