e8e57d0790144dca6df8c0d844881f4747ed2978
LiamSarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2019-01-08 10:53:34 +0000

    HAPPY NEW YEAR

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -1,65 +1,66 @@
 <?php
 
-class Product extends CI_Model
-{
-
-    function get_all_products()
-    {
-
-        $this->db->select("product_id, product_name, product_desc, product_price");
-        $query = $this->db->get('product');
-        if ($query->num_rows() > 0) {
-            return $query->result();
-        } else {
+class Product_model extends CI_Model{
+    
+    function get_all_products() { 
+          
+      $this->db->select("product_id, product_name, product_desc, product_price, image_path");
+      $query = $this->db->get('product');        
+      
+      if ($query->num_rows() > 0) {
+             
+//            return $query->result_array();
+            return $query;
+            
+         } else {
             return FALSE;
-        }
-    }
+         }
+}
 
-    function get_product_by_id($id)
-    {
-
-
+    function get_product_by_id($id){
+       
+        
 //        $id = $this->input->post('product_id');
-
-        $this->db->select("product_id, product_name, product_desc, product_price");
+        
+        $this->db->select("product_id, product_name, product_desc, product_price, product_specs, quantity, image_path");
         $this->db->where("product_id", $id);
         $query = $this->db->get('product');
-
-
+        
+        
         if ($query->num_rows() > 0) {
             return $query->row(0);
-
-        }
-
+            
+        } 
+                           
         return false;
-
-    }
+                   
+}
 
     function add_product($data)
     {
         if ($this->db->insert("product", $data)) {
             return TRUE;
-        } else {
+        }
+        else
+        {
             return TRUE;
         }
     }
-
-    function delete_product($id)
-    {
-
+    
+    function delete_product($id) { 
+                 
 //        $id = $this->input->post('id');
+                       
+        $this->db->where('product_id', $id); 
+        $this->db->delete('product'); 
+              
+}
 
-        $this->db->where('product_id', $id);
-        $this->db->delete('product');
-
-    }
-
-    function update_customer_details($data)
-    {
-
-        $this->db->where('id', $id);
-        $this->db->update('product', $data);
-    }
-
+    function update_customer_details($data){
+       
+       $this->db->where('id', $id);
+       $this->db->update('product', $data); 
+   } 
+    
 }
 
