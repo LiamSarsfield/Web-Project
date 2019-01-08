23109eb2434764dc3480c6194c16e714b80c25c5
Keith C <43409314+bluebeast78@users.noreply.github.com>
2019-01-08 10:39:51 +0100

    This is good

--- application/views/maintenance.php
+++ application/views/maintenance.php
@@ -10,29 +10,7 @@
 <body class="w3-black">
 
 <!-- Icon Bar (Sidebar - hidden on small screens) -->
-<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-  <!-- Avatar image in top left corner -->
-  <a href="index.php"><img src="<?php echo base_url(); ?>/assests/Images/MweLogo.png" style="width:100%"></a>
-  
-    <span style="font-size:30px;cursor:pointer" onclick="openNav()"  class="w3-bar-item  w3-button w3-padding-large w3-hover-black">&#9776;</span>
-    <div id="mySidenav" class="sidenav">
-  <a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="
-    fa fa-home w3-xxlarge"></i>
-    <p>HOME</p>
-  </a>
-
-    <a href="login.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-            <i class="fa fa-pencil w3-xxlarge"></i>
-            <p>Login</p>
-  </a>
-
-  <a href="signUp.php" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-        <i class="fa fa-user-plus w3-xxlarge"></i>
-        <p>Sign Up</p>
-</a>
-</div>
-</nav>
+<?php require_once('defaultmenu.php'); ?>
 
 
 <!-- Navbar on small screens (Hidden on medium and large screens) -->
--- application/views/signUp.php
+++ application/views/signUp.php
@@ -10,29 +10,7 @@
 <body class="w3-black">
 
 <!-- Icon Bar (Sidebar - hidden on small screens) -->
-<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-  <!-- Avatar image in top left corner -->
-  <a href="<?php echo site_url()?>/Home/index"><img src="<?php echo base_url(); ?>/assests/Images/MweLogo.png" style="width:100%"></a>
-  
-  <span style="font-size:30px;cursor:pointer" onclick="openNav()"  class="w3-bar-item  w3-button w3-padding-large w3-hover-black">&#9776;</span>
-  <div id="mySidenav" class="sidenav">
-<a href="<?php echo site_url()?>/Home/index" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-  <i class="
-  fa fa-home w3-xxlarge"></i>
-  <p>HOME</p>
-</a>
-
-   <a href="<?php echo site_url()?>/Customer/login" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-            <i class="fa fa-pencil w3-xxlarge"></i>
-            <p>Login</p>
-  </a>
-
-  <a href="<?php echo site_url()?>/Customer/signUp" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-        <i class="fa fa-user-plus w3-xxlarge"></i>
-        <p>Sign Up</p>
-</a>
-</div>
-</nav>
+<?php require_once('defaultmenu.php'); ?>
 
 
 <!-- Navbar on small screens (Hidden on medium and large screens) -->
