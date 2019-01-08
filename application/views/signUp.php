154574d8c63295efed1e8bf20c554c03515e3bd7
Keith C <43409314+bluebeast78@users.noreply.github.com>
2019-01-08 10:57:02 +0100

    New Files

--- application/views/signUp.php
+++ application/views/signUp.php
@@ -11,8 +11,6 @@
 
 <!-- Icon Bar (Sidebar - hidden on small screens) -->
 <?php require_once('defaultmenu.php'); ?>
-
-
 <!-- Navbar on small screens (Hidden on medium and large screens) -->
 <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
   <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
