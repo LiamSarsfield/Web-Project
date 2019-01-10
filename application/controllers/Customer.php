5d35631e9e56f26d3f9995601938c2956b59f047
K00222558 <43409483+K00222558@users.noreply.github.com>
2019-01-10 13:04:40 +0000

    update

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -18,5 +18,40 @@ class Customer extends CI_Controller {
     public function loggedIn(){
         $this->load->view('loggedin');
     }
+    
+   
+
+    public function quotes(){
+        $this->load->view('searchquotes');
+    }
+
+
+    public function viewQuote(){
+        $this->load->view('customerquote');
+    }
+
+    public function payQuote(){
+        $this->load->view('payquote');
+    }
+
+    public function declineQuote(){
+        $this->load->view('declinequote');
+    }
+
+    public function paymentSuccess(){
+        $this->load->view('paymentsuccesfulquote');
+    }
+    
+    public function view_customers() {
+        
+    }
+    
+    public function view_selected_customers($id) {
+        
+    }
+    
+    public function add_customer() {
+        
+    }
 
 }
