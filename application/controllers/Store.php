d60b4637a93b37d3b92be9b755ea89921c61f071
LiamSarsfield <43409125+LiamSarsfield@users.noreply.github.com>
2019-01-25 10:12:52 +0000

    Edited Store with Product

--- application/controllers/Store.php
+++ application/controllers/Store.php
@@ -6,7 +6,7 @@ class Store extends CI_Controller
     {
         parent::__construct();
         $account_info = $this->session->userdata('account_info') ?? NULL;
-        if (!isset($account_info['permission_status'])) {
+        if (!isset($account_info['permission_id'])) {
             redirect(site_url() . "/home");
         }
     }
@@ -16,9 +16,9 @@ class Store extends CI_Controller
 
         $data['display_block'] = "";
 
-        $this->load->model("Product");
+        $this->load->model("Product_model");
 
-        $data['query'] = $this->Product->get_all_products();
+        $data['query'] = $this->Product_model->get_all_products();
 
         //If no details found 
         if ($data['query']->num_rows() < 1) {
@@ -27,28 +27,28 @@ class Store extends CI_Controller
             //store items in the associative array $products
             foreach ($data['query']->result_array() as $product) {
                 $product_id = $product['product_id'];
-                $product_name = $product['product_name'];
-                $product_desc = $product['product_desc'];
-                $product_price = $product['product_price'];
+                $name = $product['name'];
+                $description = $product['description'];
+                $price = $product['price'];
 
                 //Create a link that when clicked calls the method GetCategoryItems in the CategoryItems_controller
                 $tag = '/Web-Project/index.php/Store/view_selected_product/' . $product_id;
 
-// 	$data['display_block'] .= "<p><strong><a href=".$tag.">".$product_name."</a></strong><br/>".$product_desc."</p>"; 
+// 	$data['display_block'] .= "<p><strong><a href=".$tag.">".$name."</a></strong><br/>".$description."</p>"; 
 
 
                 $data['display_block'] .= '<div class="w3-quarter w3-section w3-light-grey">';
                 $data['display_block'] .= '<span class="w3-xlarge"><a href="product_view.html"><img src="circuit_board.jpg" alt="circuit_board" class="w3-image" width="620" height="420"></a></span><br>';
-                $data['display_block'] .= "$product_name";
-                $data['display_block'] .= "<br>$product_desc";
-                $data['display_block'] .= "<strong>€$product_price</strong>";
+                $data['display_block'] .= "$name";
+                $data['display_block'] .= "<br>$description";
+                $data['display_block'] .= "<strong>€$price</strong>";
                 $data['display_block'] .= "<a href=" . $tag . "><button>View</button></a>";
                 $data['display_block'] .= '</div>';
             }
         }
         //Load the view passing in all the store items to be displayed
 
-        $this->load->view('store', $data);
+        $this->load->view('customer/store', $data);
 
     }
 
